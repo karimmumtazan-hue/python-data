85894a28ba8f4b02ba20f415825d0ff20b075fda
some one <karimmumtazan@gmail.com>
2026-05-27 21:44:01 +0700

    update catatan

---
+++ semester 2/django/websekolah/siswa/views.py
@@ -0,0 +1,102 @@
+
+from django.shortcuts import render, redirect
+from django.db import connection
+from django.http import HttpResponse
+from django.utils.html import escape
+
+def dictfetchall(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+
+def dictfetchone(cursor):
+    """Mengubah satu hasil query menjadi dictionary."""
+    columns = [col[0] for col in cursor.description]
+    row = cursor.fetchone()
+
+    if row is None:
+        return None
+
+    return dict(zip(columns, row))
+
+
+def siswa_list(request):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT id, nama, umur, tanggal_lahir, status_hadir, nilai_akhir
+            FROM siswa
+            ORDER BY id DESC
+        """)
+        data_siswa = dictfetchall(cursor)
+
+    search_text="bandung"
+
+    return render(request, 'list.html', {
+        'keyword': search_text,
+        'data': data_siswa
+    })
+
+
+def siswa_detail(request, id):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT * FROM siswa
+            WHERE id = %s
+            """,
+            [id]
+        )
+        siswa = dictfetchone(cursor)    
+
+    
+def siswa_create(request):
+    # cek request yg masuk, klo dia POST (submit)
+    if request.method == 'POST':        
+        # kumpulkan data dari request post
+        nama = request.POST.get('nama', '').strip()
+        umur = request.POST.get('umur', '').strip()
+        tgl_lahir = request.POST.get('tanggal_lahir', '').strip()
+        sts_hdr = request.POST.get('status_hadir', '').strip() == 'Hadir'
+        nlai_akhr = request.POST.get('nilai_akhir', '').strip()
+
+        # eksekusi query insert ke tabel siswa
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                INSERT INTO siswa (nama, umur, tanggal_lahir, status_hadir, nilai_akhir)
+                VALUES (%s, %s, %s, %s, %s)
+                """,
+                [nama, umur, tgl_lahir, sts_hdr, nlai_akhr]
+            )
+
+        # klo berhasil maka redirect ke siswa list
+        return redirect('siswa_list')
+
+    # klo gk submit (GET)
+    return render(request, 'form.html')
+
+
+def siswa_update(request, id):
+    html = f"""
+    <html>
+        <head><title>Edit Siswa</title></head>
+        <body>
+            <h1>Edit Siswa</h1>
+            <p>Edit data siswa dengan ID: {id}</p>
+        </body>
+    </html>
+    """
+    return HttpResponse(html)
+
+
+def siswa_delete(request, id):
+    html = f"""
+    <html>
+        <head><title>Hapus Siswa</title></head>
+        <body>
+            <h1>Hapus Siswa</h1>
+            <p>Yakin ingin menghapus siswa dengan ID: {id}?</p>
+        </body>
+    </html>
+    """
+    return HttpResponse(html)
